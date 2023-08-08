6a72324d6532a5bd12194e12d3592ed5e02713b4
Katlego1993 <ledwaba.katlego.kl@gmail.com>
2023-08-08 12:48:08 -0700

    all

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-a = 0
-for c in range(ord('Z'), ord('a') - 1, -1):
-    print("{}".format(chr(c - a)), end="")
-    a = 32 if a == 0 else 0
+i = 0
+for c in range(ord('z'), ord('a') - 1, -1):
+    print("{}".format(chr(c - i)), end="")
+    i = 32 if i == 0 else 0
