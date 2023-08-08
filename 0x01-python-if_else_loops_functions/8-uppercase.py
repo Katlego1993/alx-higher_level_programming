421df68af956a5d1697727b6a34677e1e69c7cc0
Katlego1993 <ledwaba.katlego.kl@gmail.com>
2023-08-08 09:34:24 -0700

    all

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
-uppercase = __import__('8-uppercase').uppercase
-
-uppercase("best")
-uppercase("Best School 98 Battery street")
+def uppercase(str):
+    for a in str:
+        if ord(a) >= 97 and ord(a) <= 122:
+            a = chr(ord(a) - 32)
+            print("{}".format(a), end="")
+            print("")
