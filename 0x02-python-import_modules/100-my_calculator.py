70c189121d02f00ecd1dff5b24fb340dee285f07
Katlego1993 <ledwaba.katlego.kl@gmail.com>
2023-08-12 09:24:35 -0700

    all

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-       from sys import argv
+    from sys import argv
     from calculator_1 import add, sub, mul, div
     argc = len(argv)
     if argc != 4:
@@ -22,4 +22,3 @@ if __name__ == "__main__":
         print('Unknown operator. Available operators: +, -, * and /')
         exit(1)
     exit(0)
-
