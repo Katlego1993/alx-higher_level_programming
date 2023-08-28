67ebbb4f826a109064fd20d1a0071385f05b529e
Katlego1993 <Ledwaba.katlego.kl@gmail.com>
2023-08-28 06:52:44 -0700

    all

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -7,7 +7,7 @@ def safe_function(fct, *args):
     try:
         result = fct(*args)
         return (result)
-    except Exception as i:
+    except:
         print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
         return (None)
 
