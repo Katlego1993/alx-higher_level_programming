2ae090d4df5c77efbcb81481ee9279931f30c7a1
Katlego1993 <Ledwaba.katlego.kl@gmail.com>
2023-08-28 07:04:37 -0700

    all

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -2,14 +2,14 @@
 
 
 def magic_calculation(a, b):
-    total = 0
+    result = 0
     for i in range(1, 3):
         try:
             if i > a:
                 raise Exception('Too far')
             else:
-                total += a ** b / i
-        except Exception:
-            total = b + a
+                result += a ** b / i
+        except:
+            result = b + a
             break
-        return (result)
+    return (result)
