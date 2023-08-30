b57eb874a9b86cbb1013cab49fe68d3a48e46356
Katlego1993 <Ledwaba.katlego.kl@gmail.com>
2023-08-29 22:56:50 -0700

    all

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Square defined."""
+
+class Square:
+    """Square representation"""
+    def__init__(self, size=0):
+        """Initialize a new Square.
+        Args:
+        size (int): Size of new Square.
+        """
+        if not isinstance(size, int):
+            raise TypeError("Size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
