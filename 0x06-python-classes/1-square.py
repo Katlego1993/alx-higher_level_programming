a7ba532fb34db22650787c7a9163906bdd299f63
Katlego1993 <Ledwaba.katlego.kl@gmail.com>
2023-08-29 23:31:35 -0700

    all

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""Square module."""
+"""Square module  defination."""
 
 
 class Square:
-    """Defines a square."""
+    """Square representation."""
 
     def __init__(self, size):
         """Constructor.
