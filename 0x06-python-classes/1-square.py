4fb62f92415344c1b5bfe38abd78e7c21e5f3214
Katlego1993 <Ledwaba.katlego.kl@gmail.com>
2023-08-29 22:50:32 -0700

    all

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Sqaure method defined"""
+class Square:
+    """Square representation"""
+    def__int__(self, size):
+        """Initialize a new Square.
+
+        Args:
+        size (int): The size of the new square.
+        """
+        self.__size = size
