63b9abfb41cbb707c23ca031951335cd44660833
Katlego1993 <Ledwaba.katlego.kl@gmail.com>
2023-09-06 06:32:39 -0700

    all

--- 0x07-python-test_driven_development/6-max_integer_test.py
+++ 0x07-python-test_driven_development/6-max_integer_test.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" The module that finds the max integer in a list
+"""Module to find the max integer in a list
 """
 
 
@@ -16,4 +16,3 @@ def max_integer(list=[]):
             result = list[i]
         i += 1
     return result
-
