936d4b6496488a033b026118f0710b0f0d9ef260
Katlego1993 <ledwaba.katlego.kl@gmail.com>
2023-11-14 04:59:50 -0800

    all

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,2 +1,9 @@
 #!/usr/bin/node
-console.log(typeof process.argv[2] === 'undefined' ? 'No argument' : process.argv[2]);
+
+const firstArg = process.argv[2];
+
+if (firstArg) {
+	console.log(firstArg);
+} else {
+	console.log('No argument');
+}
