56bfaba507e39781a7cc8c45b457e21f878a40aa
Katlego1993 <ledwaba.katlego.kl@gmail.com>
2023-11-30 10:03:28 +0000

    all

---
+++ 0x10-python-network_0/5-post_params.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# script that takes in a URL, sends a POST request to the passed URL, and displays the body of the response
+curl -s "$1" -X POST -d "email=ledwaba.katlego.kl@gmail.com&subject=I will always be here for PLD"
