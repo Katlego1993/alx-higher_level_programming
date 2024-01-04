42d1d268e0879010c3ef2e248764f11e50db9158
Katlego1993 <ledwaba.katlego.kl@gmail.com>
2024-01-04 06:33:02 +0000

    all

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -3,13 +3,12 @@
 // matches the given integer
 
 const request = require('request');
-const argv = process.argv;
-let url = 'http://swapi.co/api/films/';
 
-request(url + argv[2], function (err, res, body) {
-  if (err) {
-    console.log(err);
-  }
-  let json = JSON.parse(body);
-  console.log(json.title);
+// Construct the URL for the specific Star Wars film
+const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+
+// Use the 'request' module to perform an HTTP GET request to the constructed URL.
+request(url, function (error, response, body) {
+  // log title if successful, log error if not.
+  console.log(error || JSON.parse(body).title);
 });
